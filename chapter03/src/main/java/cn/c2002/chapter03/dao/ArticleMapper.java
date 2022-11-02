6466dc12d2f04fba55e55e56ccf3de70538b6442
dashauibi <76045471+dashauibi@users.noreply.github.com>
2022-11-02 21:14:09 +0800

    Update ArticleMapper.java

--- chapter03/src/main/java/cn/c2002/chapter03/dao/ArticleMapper.java
+++ chapter03/src/main/java/cn/c2002/chapter03/dao/ArticleMapper.java
@@ -1,7 +1,6 @@
 package cn.c2002.chapter03.dao;
 
 import cdnu.webapp.chapter03_01.pojo.Article;
-
 public interface ArticleMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -17,3 +16,6 @@ public interface ArticleMapper {
 
     int updateByPrimaryKey(Article record);
 }
+
+
+这是我在github测试的完整的项目。
